66a8b2d070db343ea7826cf288187be25681f9e2
Aakash Badgujjar <aakash.badgujjar@gmail.com>
2020-06-23 20:20:05 +0530

    Remove extra files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,20 @@
 import * as firebase from 'firebase';
 
-const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  };
-  process.env.FIREBASE_CONFIG = JSON.stringify(config);
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID:process.env.FIREBASE_APP_ID,
+FIREBASE_MEASUREMENT_ID:process.env.FIREBASE_MEASURENMENT_ID
+};
+  process.env.FIREBASE_CONFIG = JSON.stringify(firebaseConfig);
+  
+  // const admin = require("firebase-admin");
 
-  firebase.initializeApp();
+  firebase.initializeApp(firebaseConfig);
 
   const database=firebase.database();
  
